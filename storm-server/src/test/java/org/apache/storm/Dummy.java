f1ca06d4876a0c2e740bf831baf20884e1e28220
Ludovico Zarrelli <ludovico.zarr@gmail.com>
2022-05-27 21:12:26 +0200

    added dummyTest

---
+++ storm-server/src/test/java/org/apache/storm/Dummy.java
@@ -0,0 +1,12 @@
+package org.apache.storm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Dummy {
+
+    @Test
+    public void dummyMethod (){
+        Assert.assertTrue(true);
+    }
+}
