55797a4d038953e538815a61f13d68a2b7ccbddc
Ludovico Zarrelli <ludovico.zarr@gmail.com>
2022-06-02 11:11:47 +0200

    trying to solve NoClassDefFoundError

--- storm-server/src/test/java/org/apache/storm/DummyTest.java
+++ storm-server/src/test/java/org/apache/storm/DummyTest.java
@@ -3,13 +3,14 @@ package org.apache.storm;
 import org.apache.storm.healthcheck.HealthChecker;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class DummyTest {
+
 
     @Test
     public void dummyMethod (){
         HealthChecker ht = new HealthChecker();
-        assertTrue(true);
+        assertEquals(ht.getClass(), HealthChecker.class);
     }
 }
